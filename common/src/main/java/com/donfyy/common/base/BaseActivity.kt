e223eaaecf2285054114b3f578146b653a5318c7
donfyy <sven.xinxin.wang@gmail.com>
2020-10-16 18:46:22 +0800

    add more

---
+++ common/src/main/java/com/donfyy/common/base/BaseActivity.kt
@@ -0,0 +1,6 @@
+package com.donfyy.common.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+}
---
+++ common/src/main/java/com/donfyy/common/base/BaseFragment.kt
@@ -0,0 +1,5 @@
+package com.donfyy.common.base
+
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment : Fragment()
