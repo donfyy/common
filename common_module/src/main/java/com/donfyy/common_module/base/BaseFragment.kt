b1ada70a3db8839d9aa31fbd1af493653e79cca0
donfyy <sven.xinxin.wang@gmail.com>
2022-09-20 12:37:53 +0800

    add more

--- common_module/src/main/java/com/donfyy/common_module/base/BaseFragment.kt
+++ common_module/src/main/java/com/donfyy/common_module/base/BaseFragment.kt
@@ -1,5 +1,12 @@
 package com.donfyy.common_module.base
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.alibaba.android.arouter.launcher.ARouter
 
-abstract class BaseFragment : Fragment()
+abstract class BaseFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ARouter.getInstance().inject(this)
+    }
+}
